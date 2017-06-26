76f4152c5af8955bbecde50d30a19bb89a05d302
tarsoly <tarsoly@yahoo.com>
2017-06-26 16:44:36 +0200

    "move" is removed from variables' name webpanel response is a JSON file (because of this the webpanel.js is also updated)

--- js/webpanel.js
+++ js/webpanel.js
@@ -1,392 +1,359 @@
-$(document).ready(function(){	
+$(document).ready(function () {
 
-	$('#joint').show();
-	$('#tcp').hide();
-	$('#poses').hide();
+  $('#joint').show()
+  $('#tcp').hide()
+  $('#poses').hide()
 
-	$('#menu-joint').click(function(){
-		$('#joint').show();
-		$('#tcp').hide();
-		$('#poses').hide();
-	});	
+  $('#menu-joint').click(function () {
+    $('#joint').show()
+    $('#tcp').hide()
+    $('#poses').hide()
+  })  
 
-	$('#menu-tcp').click(function(){
-		$('#joint').hide();
-		$('#tcp').show();
-		$('#poses').hide();
-	});	
-	
-	$('#menu-poses').click(function(){
-		$('#joint').hide();
-		$('#tcp').hide();
-		$('#poses').show();
-	});		
-	
-	getposj();
+  $('#menu-tcp').click(function () {
+    $('#joint').hide()
+    $('#tcp').show()
+    $('#poses').hide()
+  })  
 
-	function getposj() {
+  $('#menu-poses').click(function () {
+    $('#joint').hide()
+    $('#tcp').hide()
+    $('#poses').show()
+  })    
 
-		$.ajax({
-			url: "/KAREL/webmonitor",
-			success: function (data) {
-				var data = $(data);
-				$(".joint1").html($(data).find("#joint1"));
-				$(".joint2").html($(data).find("#joint2"));
-				$(".joint3").html($(data).find("#joint3"));
-				$(".joint4").html($(data).find("#joint4"));
-				$(".joint5").html($(data).find("#joint5"));
-				$(".joint6").html($(data).find("#joint6"));
-				$(".wlx").html($(data).find("#wlx"));
-				$(".wly").html($(data).find("#wly"));
-				$(".wlz").html($(data).find("#wlz"));
-				$(".trx").html($(data).find("#trx"));
-				$(".try").html($(data).find("#try"));
-				$(".trz").html($(data).find("#trz"));				
-				setTimeout(getposj(),300);
-			},
-		});
-	};
+  getposj()
 
-	$('#abort').click(function(){
-		$.ajax({
-			type: 'GET',
-			url: '/KAREL/webabort',
-			cache: false,
-			success: function(){
-			}
-		});								
-		return false;									
-	});	
-	
-	$('#j1m').click(function(){
-		$.ajax({
-			type: 'GET',
-			url: '/KAREL/webcontrol?getstart=j1m',
-			cache: false,
-			success: function(){
-			}
-		});								
-		return false;									
-	});
+  function getposj () {
 
-	$('#j1p').click(function(){
-		$.ajax({
-			type: 'GET',
-			url: '/KAREL/webcontrol?getstart=j1p',
-			cache: false,
-			success: function(){
-			}
-		});								
-		return false;									
-	});
+    console.log('I am in the getposj function :-)')  
 
-	$('#j2m').click(function(){
-		$.ajax({
-			type: 'GET',
-			url: '/KAREL/webcontrol?getstart=j2m',
-			cache: false,
-			success: function(){
-			}
-		});								
-		return false;									
-	});
+    $.getJSON("/KAREL/webmonitor", function (data) {
+      $('.joint1').html(data.joint[0])
+      $('.joint2').html(data.joint[1])
+      $('.joint3').html(data.joint[2])
+      $('.joint4').html(data.joint[3])
+      $('.joint5').html(data.joint[4])
+      $('.joint6').html(data.joint[5])
+      $('.wlx').html(data.pose[0])
+      $('.wly').html(data.pose[1])
+      $('.wlz').html(data.pose[2])
+      $('.trx').html(data.pose[3])
+      $('.try').html(data.pose[4])
+      $('.trz').html(data.pose[5])      
+      setTimeout(getposj(), 300)
+      })
 
-	$('#j2p').click(function(){
-		$.ajax({
-			type: 'GET',
-			url: '/KAREL/webcontrol?getstart=j2p',
-			cache: false,
-			success: function(){
-			}
-		});								
-		return false;									
-	});
-	
-	$('#j3m').click(function(){
-		$.ajax({
-			type: 'GET',
-			url: '/KAREL/webcontrol?getstart=j3m',
-			cache: false,
-			success: function(){
-			}
-		});								
-		return false;									
-	});
+  }
 
-	$('#j3p').click(function(){
-		$.ajax({
-			type: 'GET',
-			url: '/KAREL/webcontrol?getstart=j3p',
-			cache: false,
-			success: function(){
-			}
-		});								
-		return false;									
-	});	
+  $('#abort').click(function () {
+    $.ajax({
+      type: 'GET',
+      url: '/KAREL/webabort',
+      cache: false,
+      success: function () {}
+    })
+    return false
+  })  
 
-	$('#j4m').click(function(){
-		$.ajax({
-			type: 'GET',
-			url: '/KAREL/webcontrol?getstart=j4m',
-			cache: false,
-			success: function(){
-			}
-		});								
-		return false;									
-	});
+  $('#j1m').click(function () {
+    $.ajax({
+      type: 'GET',
+      url: '/KAREL/webcontrol?getstart=j1m',
+      cache: false,
+      success: function () {}
+    })
+    return false
+  })
 
-	$('#j4p').click(function(){
-		$.ajax({
-			type: 'GET',
-			url: '/KAREL/webcontrol?getstart=j4p',
-			cache: false,
-			success: function(){
-			}
-		});								
-		return false;									
-	});
+  $('#j1p').click(function () {
+    $.ajax({
+      type: 'GET',
+      url: '/KAREL/webcontrol?getstart=j1p',
+      cache: false,
+      success: function () {}
+    })
+    return false
+  })
 
-	$('#j5m').click(function(){
-		$.ajax({
-			type: 'GET',
-			url: '/KAREL/webcontrol?getstart=j5m',
-			cache: false,
-			success: function(){
-			}
-		});								
-		return false;									
-	});
+  $('#j2m').click(function () {
+    $.ajax({
+      type: 'GET',
+      url: '/KAREL/webcontrol?getstart=j2m',
+      cache: false,
+      success: function () {}
+    })
+    return false
+  })
 
-	$('#j5p').click(function(){
-		$.ajax({
-			type: 'GET',
-			url: '/KAREL/webcontrol?getstart=j5p',
-			cache: false,
-			success: function(){
-			}
-		});								
-		return false;									
-	});
-	
-	$('#j6m').click(function(){
-		$.ajax({
-			type: 'GET',
-			url: '/KAREL/webcontrol?getstart=j6m',
-			cache: false,
-			success: function(){
-			}
-		});								
-		return false;									
-	});
+  $('#j2p').click(function () {
+    $.ajax({
+      type: 'GET',
+      url: '/KAREL/webcontrol?getstart=j2p',
+      cache: false,
+      success: function () {}
+    })
+    return false
+  })
 
-	$('#j6p').click(function(){
-		$.ajax({
-			type: 'GET',
-			url: '/KAREL/webcontrol?getstart=j6p',
-			cache: false,
-			success: function(){
-			}
-		});								
-		return false;									
-	});
-	
-	$('#wlpx').click(function(){
-		$.ajax({
-			type: 'GET',
-			url: '/KAREL/webcontrol?getstart=movewlpx',
-			cache: false,
-			success: function(){
-			}
-		});								
-		return false;									
-	});
+  $('#j3m').click(function () {
+    $.ajax({
+      type: 'GET',
+      url: '/KAREL/webcontrol?getstart=j3m',
+      cache: false,
+      success: function () {}
+    })
+    return false
+  })
 
-	$('#wlpy').click(function(){
-		$.ajax({
-			type: 'GET',
-			url: '/KAREL/webcontrol?getstart=movewlpy',
-			cache: false,
-			success: function(){
-			}
-		});								
-		return false;									
-	});
+  $('#j3p').click(function () {
+    $.ajax({
+      type: 'GET',
+      url: '/KAREL/webcontrol?getstart=j3p',
+      cache: false,
+      success: function () {}
+    })
+    return false
+  })  
 
-	$('#wlpz').click(function(){
-		$.ajax({
-			type: 'GET',
-			url: '/KAREL/webcontrol?getstart=movewlpz',
-			cache: false,
-			success: function(){
-			}
-		});								
-		return false;									
-	});
+  $('#j4m').click(function () {
+    $.ajax({
+      type: 'GET',
+      url: '/KAREL/webcontrol?getstart=j4m',
+      cache: false,
+      success: function () {}
+    })
+    return false
+  })
 
-	$('#wlmx').click(function(){
-		$.ajax({
-			type: 'GET',
-			url: '/KAREL/webcontrol?getstart=movewlmx',
-			cache: false,
-			success: function(){
-			}
-		});								
-		return false;									
-	});
-	
-	$('#wlmy').click(function(){
-		$.ajax({
-			type: 'GET',
-			url: '/KAREL/webcontrol?getstart=movewlmy',
-			cache: false,
-			success: function(){
-			}
-		});								
-		return false;									
-	});
-	
-	$('#wlmz').click(function(){
-		$.ajax({
-			type: 'GET',
-			url: '/KAREL/webcontrol?getstart=movewlmz',
-			cache: false,
-			success: function(){
-			}
-		});								
-		return false;									
-	});
+  $('#j4p').click(function () {
+    $.ajax({
+      type: 'GET',
+      url: '/KAREL/webcontrol?getstart=j4p',
+      cache: false,
+      success: function () {}
+    })
+    return false
+  })
 
-	$('#trpx').click(function(){
-		$.ajax({
-			type: 'GET',
-			url: '/KAREL/webcontrol?getstart=movetrpx',
-			cache: false,
-			success: function(){
-			}
-		});								
-		return false;									
-	});
+  $('#j5m').click(function () {
+    $.ajax({
+      type: 'GET',
+      url: '/KAREL/webcontrol?getstart=j5m',
+      cache: false,
+      success: function () {}
+    })
+    return false
+  })
 
-	$('#trpy').click(function(){
-		$.ajax({
-			type: 'GET',
-			url: '/KAREL/webcontrol?getstart=movetrpy',
-			cache: false,
-			success: function(){
-			}
-		});								
-		return false;									
-	});
+  $('#j5p').click(function () {
+    $.ajax({
+      type: 'GET',
+      url: '/KAREL/webcontrol?getstart=j5p',
+      cache: false,
+      success: function () {}
+    })
+    return false
+  })
 
-	$('#trpz').click(function(){
-		$.ajax({
-			type: 'GET',
-			url: '/KAREL/webcontrol?getstart=movetrpz',
-			cache: false,
-			success: function(){
-			}
-		});								
-		return false;									
-	});
+  $('#j6m').click(function () {
+    $.ajax({
+      type: 'GET',
+      url: '/KAREL/webcontrol?getstart=j6m',
+      cache: false,
+      success: function () {}
+    })
+    return false
+  })
 
-	$('#trmx').click(function(){
-		$.ajax({
-			type: 'GET',
-			url: '/KAREL/webcontrol?getstart=movetrmx',
-			cache: false,
-			success: function(){
-			}
-		});								
-		return false;									
-	});
-	
-	$('#trmy').click(function(){
-		$.ajax({
-			type: 'GET',
-			url: '/KAREL/webcontrol?getstart=movetrmy',
-			cache: false,
-			success: function(){
-			}
-		});								
-		return false;									
-	});
-	
-	$('#trmz').click(function(){
-		$.ajax({
-			type: 'GET',
-			url: '/KAREL/webcontrol?getstart=movetrmz',
-			cache: false,
-			success: function(){
-			}
-		});								
-		return false;									
-	});
+  $('#j6p').click(function () {
+    $.ajax({
+      type: 'GET',
+      url: '/KAREL/webcontrol?getstart=j6p',
+      cache: false,
+      success: function () {}
+    })
+    return false
+  })
 
-	$('#pickup').click(function(){
-		$.ajax({
-			type: 'GET',
-			url: '/KAREL/webcontrol?getstart=pickup',
-			cache: false,
-			success: function(){
-			}
-		});								
-		return false;									
-	});
-	
-	$('#zero').click(function(){
-		$.ajax({
-			type: 'GET',
-			url: '/KAREL/webcontrol?getstart=zero',
-			cache: false,
-			success: function(){
-			}
-		});								
-		return false;									
-	});
+  $('#wlpx').click(function () {
+    $.ajax({
+      type: 'GET',
+      url: '/KAREL/webcontrol?getstart=wlpx',
+      cache: false,
+      success: function () {}
+    })
+    return false
+  })
 
-	$('#place').click(function(){
-		$.ajax({
-			type: 'GET',
-			url: '/KAREL/webcontrol?getstart=place',
-			cache: false,
-			success: function(){
-			}
-		});								
-		return false;									
-	});
+  $('#wlpy').click(function () {
+    $.ajax({
+      type: 'GET',
+      url: '/KAREL/webcontrol?getstart=wlpy',
+      cache: false,
+      success: function () {}
+    })
+    return false
+  })
 
-	$('#coord').click(function(){
-		$.ajax({
-			type: 'GET',
-			url: '/KAREL/webcontrol?getstart=coord',
-			cache: false,
-			success: function(){
-			}
-		});								
-		return false;									
-	});
+  $('#wlpz').click(function () {
+    $.ajax({
+      type: 'GET',
+      url: '/KAREL/webcontrol?getstart=wlpz',
+      cache: false,
+      success: function () {}
+    })
+    return false
+  })
 
-	$('#show').click(function(){
-		$.ajax({
-			type: 'GET',
-			url: '/KAREL/webcontrol?getstart=show',
-			cache: false,
-			success: function(){
-			}
-		});								
-		return false;									
-	});
+  $('#wlmx').click(function () {
+    $.ajax({
+      type: 'GET',
+      url: '/KAREL/webcontrol?getstart=wlmx',
+      cache: false,
+      success: function () {}
+    })
+    return false
+  })
 
-	$('#touch').click(function(){
-		$.ajax({
-			type: 'GET',
-			url: '/KAREL/webcontrol?getstart=touch',
-			cache: false,
-			success: function(){
-			}
-		});								
-		return false;									
-	});
-	
-});
+  $('#wlmy').click(function () {
+    $.ajax({
+      type: 'GET',
+      url: '/KAREL/webcontrol?getstart=wlmy',
+      cache: false,
+      success: function () {}
+    })
+    return false
+  })
 
+  $('#wlmz').click(function () {
+    $.ajax({
+      type: 'GET',
+      url: '/KAREL/webcontrol?getstart=wlmz',
+      cache: false,
+      success: function () {}
+    })
+    return false
+  })
+
+  $('#trpx').click(function () {
+    $.ajax({
+      type: 'GET',
+      url: '/KAREL/webcontrol?getstart=trpx',
+      cache: false,
+      success: function () {}
+    })
+    return false
+  })
+
+  $('#trpy').click(function () {
+    $.ajax({
+      type: 'GET',
+      url: '/KAREL/webcontrol?getstart=trpy',
+      cache: false,
+      success: function () {}
+    })
+    return false
+  })
+
+  $('#trpz').click(function () {
+    $.ajax({
+      type: 'GET',
+      url: '/KAREL/webcontrol?getstart=trpz',
+      cache: false,
+      success: function () {}
+    })
+    return false
+  })
+
+  $('#trmx').click(function () {
+    $.ajax({
+      type: 'GET',
+      url: '/KAREL/webcontrol?getstart=trmx',
+      cache: false,
+      success: function () {}
+    })
+    return false
+  })
+
+  $('#trmy').click(function () {
+    $.ajax({
+      type: 'GET',
+      url: '/KAREL/webcontrol?getstart=trmy',
+      cache: false,
+      success: function () {}
+    })
+    return false
+  })
+
+  $('#trmz').click(function () {
+    $.ajax({
+      type: 'GET',
+      url: '/KAREL/webcontrol?getstart=trmz',
+      cache: false,
+      success: function () {}
+    })
+    return false
+  })
+
+  $('#pickup').click(function () {
+    $.ajax({
+      type: 'GET',
+      url: '/KAREL/webcontrol?getstart=pickup',
+      cache: false,
+      success: function () {}
+    })
+    return false
+  })
+
+  $('#zero').click(function () {
+    $.ajax({
+      type: 'GET',
+      url: '/KAREL/webcontrol?getstart=zero',
+      cache: false,
+      success: function () {}
+    })
+    return false
+  })
+
+  $('#place').click(function () {
+    $.ajax({
+      type: 'GET',
+      url: '/KAREL/webcontrol?getstart=place',
+      cache: false,
+      success: function () {}
+    })
+    return false
+  })
+
+  $('#coord').click(function () {
+    $.ajax({
+      type: 'GET',
+      url: '/KAREL/webcontrol?getstart=coord',
+      cache: false,
+      success: function () {}
+    })
+    return false
+  })
+
+  $('#show').click(function () {
+    $.ajax({
+      type: 'GET',
+      url: '/KAREL/webcontrol?getstart=show',
+      cache: false,
+      success: function () {}
+    })
+    return false
+  })
+
+  $('#touch').click(function () {
+    $.ajax({
+      type: 'GET',
+      url: '/KAREL/webcontrol?getstart=touch',
+      cache: false,
+      success: function () {}
+    })
+    return false
+  })
+
+})
